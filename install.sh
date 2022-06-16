17c1720ec0dfd12fab658a348588943ad54a55c1
Nima <nima.sa@icloud.com>
2022-06-16 13:10:42 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 python3 -m pip install --upgrade pip virtualenv
 echo export PATH="/home/pi/.local/bin:$PATH" > ~/.bashrc
 source ~/.bashrc
-cd Desktop/rpiKeyboard
+cd ~/Desktop/rpiKeyboard
 virtualenv rpienv
 source rpienv/bin/activate
 pip install -r requirements.txt
