9da537ae08b0df25c695a6c4c566092811dd783e
Nima <nima.sa@icloud.com>
2022-07-31 16:20:23 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mv ~/Desktop/raspberry-pi-socket-keyboard ~/Desktop/rpiKeyboard
 python3 -m pip install --upgrade pip virtualenv
 echo export PATH="/home/pi/.local/bin:$PATH" > ~/.bashrc
 source ~/.bashrc
@@ -5,6 +6,6 @@ cd ~/Desktop/rpiKeyboard
 virtualenv rpienv
 source rpienv/bin/activate
 pip install -r requirements.txt
-cp rpikeyboard.service /lib/systemd/system/rpikeyboard.service
+sudo cp rpikeyboard.service /lib/systemd/system/rpikeyboard.service
 systemctl enable rpikeyboard
 
